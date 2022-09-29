41143ec13fcf220a5bdf87a53b479423b538657a
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-29 18:43:35 -0300

    CSS-in-JS - Finalizando lista de Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -6,9 +6,11 @@ import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
-import { PlayerCard } from "@components/PlayerCard";
 
 export function Players() {
   const [team, setTeam] = useState("Time A");
@@ -51,7 +53,18 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={<ListEmpty message="Não há pessoas nesse time" />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          {
+            paddingBottom: 20
+          },
+          players.length === 0 && {
+            flex: 1
+          }
+        ]}
       />
+      <Button title="Remover Turma" type="SECONDARY" />
     </Container>
   );
 }

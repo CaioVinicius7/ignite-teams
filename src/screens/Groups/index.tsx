d8692abaf02c73fd2522718227d9b2d206cc879b
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-29 23:07:55 -0300

    Navegação - Tipando as rotas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
+import { FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
-
-import { Container } from "./styles";
-import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
+
+import { Container } from "./styles";
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([
     "Galera do Ignite",
     "Amigos"
   ]);
+
+  const navigation = useNavigation();
+
+  function handleNewGroup() {
+    navigation.navigate("players", {
+      group: "Galera do Ignite"
+    });
+  }
 
   return (
     <Container>
@@ -35,7 +44,7 @@ export function Groups() {
         }
       />
 
-      <Button title="Criar nova turma" />
+      <Button title="Criar nova turma" onPress={handleNewGroup} />
     </Container>
   );
 }

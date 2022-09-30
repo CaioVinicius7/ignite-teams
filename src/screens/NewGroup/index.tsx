a88da3cc17b8a62974fb52ce3c00e289d8228356
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-29 23:23:44 -0300

    Navegação - Implementando o voltar

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,10 +1,21 @@
+import { useNavigation } from "@react-navigation/native";
+
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", {
+      group: "Galera do Ignite"
+    });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -23,6 +34,7 @@ export function NewGroup() {
           style={{
             marginTop: 20
           }}
+          onPress={handleNew}
         />
       </Content>
     </Container>

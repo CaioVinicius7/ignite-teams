67ede3fb0601770cc0d6bc4b64379b3ea896a0f9
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-29 23:48:02 -0300

    Navegação - Prevenindo Glitch na navegação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,22 @@
+import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./app.routes";
+import { useTheme } from "styled-components/native";
 
 export function Routes() {
+  const { colors } = useTheme();
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: colors.gray_600
+      }}
+    >
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   );
 }

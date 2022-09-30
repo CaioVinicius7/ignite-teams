72a063efaed50d4de2699ffeec63132900ec2218
Caio Vinícius <caio1525pereira@gmail.com>
2022-09-30 10:58:41 -0300

    Local Storage - Obtendo os grupos

--- src/storage/group/groupsCreate.ts
+++ src/storage/group/groupsCreate.ts
@@ -2,9 +2,15 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { GROUP_COLLECTION } from "@storage/storageConfig";
 
+import { groupsGetAll } from "./groupsGetAll";
+
 export async function groupCreate(newGroupName: string) {
   try {
-    await AsyncStorage.setItem(GROUP_COLLECTION, newGroupName);
+    const storageGroups = await groupsGetAll();
+
+    const storage = JSON.stringify([...storageGroups, newGroupName]);
+
+    await AsyncStorage.setItem(GROUP_COLLECTION, storage);
   } catch (error) {
     throw error;
   }

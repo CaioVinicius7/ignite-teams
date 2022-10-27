7e3869d779e9c7fcd8f092bd7394de71cdcd31a2
Caio Vinícius <caio1525pereira@gmail.com>
2022-10-27 11:20:58 -0300

    Local Storage - Arrumando importação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { useTheme } from "styled-components/native";
 
 import { AppRoutes } from "./app.routes";
-import { useTheme } from "styled-components/native";
 
 export function Routes() {
   const { colors } = useTheme();
